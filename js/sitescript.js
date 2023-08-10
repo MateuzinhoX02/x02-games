4fd0bb417978fea24df59754ce37351332140acb
Mateus Soares <105178173+MateuzinhoX02@users.noreply.github.com>
2023-08-10 19:19:31 -0300

    Update sitescript.js

--- js/sitescript.js
+++ js/sitescript.js
@@ -1,3 +1,3 @@
 function redirect() {
-  window.location.href = "https://mateuzinhox02.github.io/x02-games/index.html";
+  window.location.href = "https://mateuzinhox02.github.io/x02-games/home.html";
 }
